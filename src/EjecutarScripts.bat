e2d693db5ae5e4447f52ad8394d1829bdaf289ac
Gonzalo Coca Rocha <89556185+Crokidokis@users.noreply.github.com>
2023-10-29 14:00:44 -0300

    Delete src directory

--- src/EjecutarScripts.bat
+++
@@ -1,4 +0,0 @@
-echo. ##############TEST PATH #############
-cd "C:\Users\Gonzalo\Desktop\Practica Automation\Practica Automation\src\Tests"
-python -m pytest tst_001.py --html=../Results/CocaRochaGonzalo.html --self-contained-html
-pause
